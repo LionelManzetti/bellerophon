5a315d803b603cfe0ebea686b8b99e83fedde0ae
TheArtyMorty <38098700+TheArtyMorty@users.noreply.github.com>
2023-09-18 12:48:36 +0200

    luc first commit :)
    j'ai fait des trucs :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./styles/stars.css";
 import Login from "./components/pages/Login";
 import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -6,6 +7,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
 	return (
 		<div id="root">
+			<div className="background-container">
+				<div className="stars" />
+				<div className="twinkling" />
+			</div>
 			<Router>
 				<Routes>
 					<Route path="/nostromo/:userId" element={<Home />} />
