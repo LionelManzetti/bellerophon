ac9339c4c7fed5f9abd7f7d72a3a32bc41b16e59
TheArtyMorty <38098700+TheArtyMorty@users.noreply.github.com>
2023-10-12 13:09:30 +0200

    wip

--- src/components/puzzles/ConnexionPuzzle.jsx
+++ src/components/puzzles/ConnexionPuzzle.jsx
@@ -3,29 +3,63 @@ import ConnexionItem from './elements/ConnexionItem';
 import Target from './elements/Target';
 
 const ConnexionPuzzle = () => {
+  const targetContent = [];
+  targetContent.push(<Target type={1} position={1} />);
+  targetContent.push(<Target type={0} position={2} />);
+  targetContent.push(<Target type={-1} position={3} />);
+  targetContent.push(<Target type={-1} position={4} />);
+  targetContent.push(<Target type={-1} position={5} />);
+  targetContent.push(<Target type={-1} position={6} />);
+  targetContent.push(<Target type={1} position={7} />);
+  targetContent.push(<Target type={-1} position={8} />);
+  targetContent.push(<Target type={-1} position={9} />);
+  targetContent.push(<Target type={-1} position={10} />);
+  targetContent.push(<Target type={0} position={11} />);
+  targetContent.push(<Target type={-1} position={12} />);
+
+  const gridContent = [
+    {
+      position: [1, 1],
+      type: -1,
+      rotation: 0,
+    },
+    {
+      position: [1, 2],
+      type: -1,
+      rotation: 0,
+    },
+  ];
+
+  const generatePuzzleGrid = (content) => {
+    return content.map((content, index) => {
+      return <ConnexionItem content={content} key={index} />;
+    });
+  };
+
+  const onReset = (e) => {
+    e.preventDefault();
+    window.location.reload(false);
+  };
+
+  const onValidate = (e) => {
+    e.preventDefault();
+    gridContent.forEach((element) => {
+      console.log(element);
+    });
+  };
+
   return (
-    <div className="grid-container">
-      <ConnexionItem type={1} position={[1, 1]} rotation={0} />
-      <ConnexionItem type={1} position={[1, 2]} rotation={1} />
-      <ConnexionItem type={1} position={[1, 3]} rotation={2} />
-      <ConnexionItem type={1} position={[2, 1]} rotation={3} />
-      <ConnexionItem type={-1} position={[2, 2]} rotation={0} />
-      <ConnexionItem type={-1} position={[2, 3]} rotation={0} />
-      <ConnexionItem type={-1} position={[3, 1]} rotation={0} />
-      <ConnexionItem type={-1} position={[3, 2]} rotation={0} />
-      <ConnexionItem type={-1} position={[3, 3]} rotation={0} />
-      <Target type={1} position={1} />
-      <Target type={0} position={2} />
-      <Target type={-1} position={3} />
-      <Target type={-1} position={4} />
-      <Target type={-1} position={5} />
-      <Target type={-1} position={6} />
-      <Target type={1} position={7} />
-      <Target type={-1} position={8} />
-      <Target type={-1} position={9} />
-      <Target type={-1} position={10} />
-      <Target type={0} position={11} />
-      <Target type={-1} position={12} />
+    <div className="container">
+      <div className="grid-container">
+        {generatePuzzleGrid(gridContent)}
+        {targetContent}
+      </div>
+      <button className="button" onClick={onReset}>
+        Remise à zéro
+      </button>
+      <button className="button" onClick={onValidate}>
+        Valider les connections
+      </button>
     </div>
   );
 };
--- src/components/puzzles/elements/ConnexionItem.jsx
+++ src/components/puzzles/elements/ConnexionItem.jsx
@@ -6,8 +6,110 @@ import Cable3 from '../images/Cable3.png';
 import Cable4 from '../images/Cable4.png';
 import Cable5 from '../images/Cable5.png';
 import '../../../styles/puzzles.css';
+import { useState } from 'react';
 
-const ConnexionItem = ({ type = 0, position = [1, 1], rotation = 0 }) => {
+const ConnexionItem = ({ content }) => {
+  const [userCode, setUserCode] = useState('');
+
+  let { position, type, rotation } = content;
+
+  const pos = position[1] + (position[0] - 1) * 3;
+  const divClassName = 'grid-item' + pos.toString() + ' grid-item';
+
+  switch (userCode) {
+    // courbe
+    case 'CC1':
+      type = 1;
+      rotation = 0;
+      break;
+    case 'CC2':
+      type = 1;
+      rotation = 1;
+      break;
+    case 'CC3':
+      type = 1;
+      rotation = 2;
+      break;
+    case 'CC4':
+      type = 1;
+      rotation = 3;
+      break;
+    // ligne droite
+    case 'LL1':
+      type = 0;
+      rotation = 0;
+      break;
+    case 'LL2':
+      type = 0;
+      rotation = 1;
+      break;
+    // Intersection en T
+    case 'TT1':
+      type = 2;
+      rotation = 0;
+      break;
+    case 'TT2':
+      type = 2;
+      rotation = 1;
+      break;
+    case 'TT3':
+      type = 2;
+      rotation = 2;
+      break;
+    case 'TT4':
+      type = 2;
+      rotation = 3;
+      break;
+    // End
+    case 'EE1':
+      type = 5;
+      rotation = 0;
+      break;
+    case 'EE2':
+      type = 5;
+      rotation = 1;
+      break;
+    case 'EE3':
+      type = 5;
+      rotation = 2;
+      break;
+    case 'EE4':
+      type = 5;
+      rotation = 3;
+      break;
+    // Croix
+    case 'XX1':
+      type = 4;
+      rotation = 0;
+      break;
+    // double courbe
+    case 'DC1':
+      type = 3;
+      rotation = 0;
+      break;
+    case 'DC2':
+      type = 3;
+      rotation = 1;
+      break;
+  }
+
+  if (type < 0) {
+    return (
+      <div className={divClassName}>
+        <input
+          className="input-code"
+          type="text"
+          maxLength="5"
+          value={userCode}
+          onChange={(e) => {
+            setUserCode(e.target.value.toLocaleUpperCase());
+          }}
+          placeholder="---"
+        />
+      </div>
+    );
+  }
+
   let imgClassName = 'img rotate' + (rotation * 90).toString();
   let img = <img className={imgClassName}></img>;
   switch (type) {
@@ -32,8 +134,7 @@ const ConnexionItem = ({ type = 0, position = [1, 1], rotation = 0 }) => {
     default:
       img = <img className={imgClassName} src={Empty} r />;
   }
-  const pos = position[1] + (position[0] - 1) * 3;
-  const divClassName = 'grid-item' + pos.toString() + ' grid-item';
+
   return <div className={divClassName}>{img}</div>;
 };
 
