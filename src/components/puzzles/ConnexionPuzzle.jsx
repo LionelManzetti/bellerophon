f771eb3f89974765cb6d8d770d064ed1a35b4141
TheArtyMorty <38098700+TheArtyMorty@users.noreply.github.com>
2023-10-19 17:40:42 +0200

    logic WIP for puzzle validation

--- src/components/puzzles/ConnexionPuzzle.jsx
+++ src/components/puzzles/ConnexionPuzzle.jsx
@@ -81,9 +81,171 @@ const ConnexionPuzzle = ({ targets, centerTile }) => {
     window.location.reload(false);
   };
 
+  const GetTileConnexions = (tile) => {
+    //Array of connexions [0,1,...] 0=top, 1=right, 2= bottom, 3=left
+    switch (tile.type) {
+      case 0: //ligne
+        switch (tile.rotation) {
+          case 0:
+            return [0, 2];
+          case 1:
+          default:
+            return [1, 3];
+        }
+      case 1: //arc
+        switch (tile.rotation) {
+          case 0:
+            return [1, 2];
+          case 1:
+            return [2, 3];
+          case 2:
+            return [3, 0];
+          case 3:
+          default:
+            return [0, 1];
+        }
+      case 2: // T
+        switch (tile.rotation) {
+          case 0:
+            return [1, 2, 3];
+          case 1:
+            return [2, 3, 0];
+          case 2:
+            return [3, 0, 1];
+          case 3:
+          default:
+            return [0, 1, 2];
+        }
+      case 3: // double courbe
+        return [0, 1, 2, 3];
+      case 4: // croisement
+        return [0, 1, 2, 3];
+      case 5: // cul de sac
+        switch (tile.rotation) {
+          case 0:
+            return [2];
+          case 1:
+            return [3];
+          case 2:
+            return [0];
+          case 3:
+          default:
+            return [1];
+        }
+    }
+    return [];
+  };
+
+  const GetTileNameFromType = (type) => {
+    switch (type) {
+      case 0:
+        return 'Ligne droite';
+      case 1:
+        return 'Courbe';
+      case 2:
+        return 'Intersection en T';
+      case 3:
+        return 'Double courbe';
+      case 4:
+        return 'Croisement en X';
+      case 5:
+      default:
+        return 'Fin de cable';
+    }
+  };
+
   const onValidate = (e) => {
     e.preventDefault();
-    console.log(grid);
+    //test if any grid tile is not set
+    const numberOfTilesNotSet = grid.filter((t) => t.type == -1).length;
+    if (numberOfTilesNotSet > 0) {
+      alert(
+        'Il manque ' +
+          numberOfTilesNotSet +
+          ' connexion' +
+          (numberOfTilesNotSet == 1 ? ' !' : 's !'),
+      );
+      return;
+    }
+    //test number of tiles types
+    for (let i = 0; i < 3; i++) {
+      const numberOfTilesOfThisType = grid.filter((t) => t.type == i).length;
+      if (numberOfTilesOfThisType > 3) {
+        alert(
+          'Trop de connexion de type ' +
+            GetTileNameFromType(i) +
+            ' utilisées (max 3) !',
+        );
+        return;
+      }
+    }
+    for (let i = 3; i < 6; i++) {
+      const numberOfTilesOfThisType = grid.filter((t) => t.type == i).length;
+      if (numberOfTilesOfThisType > 2) {
+        alert(
+          'Trop de connexion de type ' +
+            GetTileNameFromType(i) +
+            ' utilisées (max 1) !',
+        );
+        return;
+      }
+    }
+    //Test if there are any wrong connexion
+    let anyWrongConnexion = false;
+    for (let i = 0; i < 9; i++) {
+      const tile = grid[i];
+      const connexions = GetTileConnexions(tile);
+      connexions.forEach((c) => {
+        //test wall connexions
+        if (c == 0 && i <= 2) {
+          if (!targets.includes(i + 1)) {
+            alert(
+              'La tuile ' + (i + 1) + ' est connectée à un mur (vers le haut).',
+            );
+            anyWrongConnexion = true;
+            return;
+          }
+        }
+        if (c == 1 && (i + 1) % 3 == 0) {
+          if (!targets.includes(4 + (i % 3))) {
+            alert(
+              'La tuile ' +
+                (i + 1) +
+                ' est connectée à un mur (vers la droite).',
+            );
+            anyWrongConnexion = true;
+          }
+        }
+        if (c == 2 && i >= 6) {
+          if (!targets.includes(15 - i)) {
+            alert(
+              'La tuile ' + (i + 1) + ' est connectée à un mur (vers le bas).',
+            );
+            anyWrongConnexion = true;
+          }
+        }
+        if (c == 3 && i % 3 == 0) {
+          if (!targets.includes(12 - i / 3)) {
+            alert(
+              'La tuile ' +
+                (i + 1) +
+                ' est connectée à un mur (vers la gauche).',
+            );
+            anyWrongConnexion = true;
+          }
+        }
+        //test internal connexions
+        // TODOOOOOOOOOOOOOOOOOO
+      });
+      if (anyWrongConnexion) {
+        return;
+      }
+    }
+    //Test if same types computer are properly linked
+    // TOODDDDDDOOOOOOOOOOOOO
+
+    //If no failures before, we are good
+    alert('Ca semble bon gros !!!');
   };
 
   return (
