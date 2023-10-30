5441a0ccec35dfeb19f13fc1f9d25601b7aed6d5
TheArtyMorty <38098700+TheArtyMorty@users.noreply.github.com>
2023-10-30 16:03:53 +0100

    logos

--- src/components/roles/Base.jsx
+++ src/components/roles/Base.jsx
@@ -1,10 +1,59 @@
+import Responsable from './Logos/Responsable.png';
+import Medical from './Logos/Médical.png';
+import Colon from './Logos/Colon.png';
+import Eugéniste from './Logos/Eugéniste.png';
+import Clandestin from './Logos/Clandestin.png';
+import Navigateur from './Logos/Navigateur.png';
+import Politique from './Logos/Politique.png';
+import Sécurité from './Logos/Sécurité.png';
+import Technicien from './Logos/Technicien.png';
+import Pontife from './Logos/Pontife.png';
+
 const Base = ({ currentUser }) => {
-  const { firstName, lastName, age, gender, job, specialty, helperMessage } =
-    currentUser;
+  const {
+    firstName,
+    lastName,
+    age,
+    gender,
+    job,
+    specialty,
+    helperMessage,
+    logo,
+  } = currentUser;
+
+  const GetLogo = () => {
+    switch (logo) {
+      case 'Médical':
+        return <img className="roles-img" src={Medical}></img>;
+      case 'Clandestin':
+        return <img className="roles-img" src={Clandestin}></img>;
+      case 'Eugéniste':
+        return <img className="roles-img" src={Eugéniste}></img>;
+      case 'Responsable':
+        return <img className="roles-img" src={Responsable}></img>;
+      case 'Technicien':
+        return <img className="roles-img" src={Technicien}></img>;
+      case 'Navigateur':
+        return <img className="roles-img" src={Navigateur}></img>;
+      case 'Sécurité':
+        return <img className="roles-img" src={Sécurité}></img>;
+      case 'Politique':
+        return <img className="roles-img" src={Politique}></img>;
+      case 'Pontife':
+        return <img className="roles-img" src={Pontife}></img>;
+      case 'Colon':
+      default:
+        return <img className="roles-img" src={Colon}></img>;
+    }
+  };
+
   return (
     <div className="roles-container">
-      <div className="roles-title">
-        Bienvenue {firstName} {lastName}
+      <div className="roles-lines">
+        {GetLogo()}
+        <div className="roles-title">
+          Bienvenue {firstName} {lastName}
+        </div>
       </div>
       <div className="roles-lines">
         <div className="roles-content">Nom : {lastName}</div>
